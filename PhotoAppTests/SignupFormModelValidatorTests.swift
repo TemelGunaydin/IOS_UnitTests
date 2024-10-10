bee19a1e0ec805d5c814f07e4cc7c8fb342fbde9
Temel Gunaydin <tgunaydin@gmail.com>
2024-10-10 19:00:43 +0300

    Setup function and teardown is used for the first time
    We made a refactor for the code. Because we are using builtin functions
    now.

--- PhotoAppTests/SignupFormModelValidatorTests.swift
+++ PhotoAppTests/SignupFormModelValidatorTests.swift
@@ -2,17 +2,24 @@
 import XCTest
 
 final class SignupFormModelValidatorTests: XCTestCase {
-    override func setUpWithError() throws {}
+    var sut: SignupFormModelValidator!
 
-    override func tearDownWithError() throws {}
+    override func setUpWithError() throws {
+        // Initialize the sut
+        sut = SignupFormModelValidator()
+    }
+
+    override func tearDownWithError() throws {
+        // Deinitialize the sut
+        // This is important to avoid memory leaks
+        // This method run after each test method
+        sut = nil
+    }
 
     // Test case 1
     // When an empty first name is provided, the function should return FALSE
     // test method name should start with "test" then the name of the method or class to be tested, then the scenario, then the expected behavior
     func testSignupFormModelValidator_WhenValidFirstNameProvided_ShouldReturnTrue() {
-        // Arrange
-        let sut = SignupFormModelValidator()
-
         // Act
         let isFirstNameValid = sut.isFirstNameValid(firstName: "Temel")
 
@@ -25,9 +32,6 @@ final class SignupFormModelValidatorTests: XCTestCase {
     // Test case 2
     // When a short first name is provided, the function should return FALSE
     func testSignupFormModelValidator_WhenTooShortFirstNameProvided_ShouldReturnFalse() {
-        // Arrange
-        let sut = SignupFormModelValidator()
-
         // Act
         let isFirstNameValid = sut.isFirstNameValid(firstName: "T")
 
